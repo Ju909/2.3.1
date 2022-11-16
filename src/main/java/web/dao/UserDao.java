35f194cdcc26ae018517dcaf47130507c85ee1a5
Юлия Кулакова <111242505+Ju909@users.noreply.github.com>
2022-11-16 13:06:52 +0400

    Add files via upload

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,19 @@
+package web.dao;
+
+import web.model.User;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void addUser(User user);
+
+    void removeUserById(long id);
+
+    List<User> getAllUsers();
+
+    User getUserById(long id);
+
+    void updateUser(Long id, User user);
+}
